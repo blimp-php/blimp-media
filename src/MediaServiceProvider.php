44ab26cf896490a276dbac3684a84e81aaea4e42
Nelson Antunes <nelson.antunes@gmail.com>
2015-06-23 03:51:04 +0100

    File upload support

--- src/MediaServiceProvider.php
+++ src/MediaServiceProvider.php
@@ -1,64 +1,59 @@
 <?php
+
 namespace Blimp\Media;
 
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
-use Symfony\Component\Templating\Helper\SlotsHelper;
-use Symfony\Component\Templating\Loader\FilesystemLoader;
-use Symfony\Component\Templating\PhpEngine;
-use Symfony\Component\Templating\TemplateNameParser;
-
 use Blimp\Media\Documents\Media;
 
-class MediaServiceProvider implements ServiceProviderInterface {
-    public function register(Container $api) {
-        $api['media.http.path'] = '';
-        $api['media.physical.path'] = __DIR__;
-
-        $api['media.store'] = $api->protect(function ($uploadedFile, $bucket, $filename = null, $extension = null) use($api) {
-            if(empty($filename)) {
+class MediaServiceProvider implements ServiceProviderInterface
+{
+    public function register(Container $api)
+    {
+        $api['media.store'] = $api->protect(function ($uploadedFile, $bucket, $filename = null, $extension = null) use ($api) {
+            if (empty($filename)) {
                 $filename = sha1(uniqid(mt_rand(), true));
             }
 
-            if(empty($extension)) {
+            if (empty($extension)) {
                 $extension = $uploadedFile->guessExtension();
 
-                if(empty($extension)) {
+                if (empty($extension)) {
                     $extension = 'bin';
                 }
             }
 
-            $file = $uploadedFile->move($api['media.physical.path'] . '/' . $bucket, $filename . '.' . $extension);
-
-            $dm = $api['dataaccess.mongoodm.documentmanager']();
-
-            $media = new Media();
-            $media->setBasePath($api['media.physical.path']);
-            $media->setBucket($bucket);
-            $media->setFilePath($filename . '.' . $extension);
-
-            $dm->persist($media);
-
-            return $media;
+            $stream = fopen($uploadedFile->getRealPath(), 'r+');
+            $api['media.filesystems']['user']->writeStream($bucket.'/'.$filename.'.'.$extension, $stream, [
+                'visibility' => 'public',
+                'mimetype' => $uploadedFile->getMimeType(),
+            ]);
+            fclose($stream);
+                
+            return [
+                'bucket' => $bucket,
+                'file' => $filename.'.'.$extension,
+                'originalName' => $uploadedFile->getClientOriginalName()
+            ];
         });
 
-        $api['media.store.download'] = $api->protect(function ($url, $bucket, $filename = null, $extension = null) use($api) {
-            if(empty($filename)) {
+        $api['media.store.download'] = $api->protect(function ($url, $bucket, $filename = null, $extension = null) use ($api) {
+            if (empty($filename)) {
                 $filename = sha1(uniqid(mt_rand(), true));
             }
 
-            if(empty($extension)) {
+            if (empty($extension)) {
                 $extension = 'bin';
             }
 
-            file_put_contents($api['media.physical.path'] . '/' . $bucket . '/' . $filename . '.' . $extension, fopen($url, 'r'));
+            file_put_contents($api['media.physical.path'].'/'.$bucket.'/'.$filename.'.'.$extension, fopen($url, 'r'));
 
             $dm = $api['dataaccess.mongoodm.documentmanager']();
 
             $media = new Media();
             $media->setBasePath($api['media.physical.path']);
             $media->setBucket($bucket);
-            $media->setFilePath($filename . '.' . $extension);
+            $media->setFilePath($filename.'.'.$extension);
 
             $dm->persist($media);
 
@@ -66,10 +61,10 @@ class MediaServiceProvider implements ServiceProviderInterface {
         });
 
         $api->extend('blimp.extend', function ($status, $api) {
-            if($status) {
+            if ($status) {
                 if ($api->offsetExists('dataaccess.mongoodm.mappings')) {
                     $api->extend('dataaccess.mongoodm.mappings', function ($mappings, $api) {
-                        $mappings[] = ['dir' => __DIR__ . '/Documents', 'prefix' => 'Blimp\\Media\\Documents\\'];
+                        $mappings[] = ['dir' => __DIR__.'/Documents', 'prefix' => 'Blimp\\Media\\Documents\\'];
 
                         return $mappings;
                     });
--- src/Rest/Media.php
+++ src/Rest/Media.php
@@ -1,45 +1,102 @@
 <?php
+
 namespace Blimp\Media\Rest;
 
 use Pimple\Container;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Blimp\Http\BlimpHttpException;
+use League\Glide\ServerFactory;
 
-use Blimp\DataAccess\Rest\MongoODMResource;
+class Media
+{
+    public function process(Container $api, Request $request, $id, $field, $index = -1, $_securityDomain = null, $_resourceClass = null, $parent_id = null, $_parentIdField = null, $_parentResourceClass = null)
+    {
+        if ($_resourceClass == null) {
+            throw new BlimpHttpException(Response::HTTP_INTERNAL_SERVER_ERROR, 'Resource class not specified');
+        }
 
-class Media {
-    public function process(Container $api, Request $request, $id, $_bucket, $_securityDomain = null) {
+        $token = null;
+        if ($api->offsetExists('security')) {
+            $token = $api['security']->getToken();
+        }
+        $user = $token !== null ? $token->getUser() : null;
+
         switch ($request->getMethod()) {
             case 'GET':
-                $token = $api['security']->getToken();
-                $user = $token !== null ? $token->getUser() : null;
+                $index = intval($index);
 
-                $contentLang = $api['http.utils']->guessContentLang($request->getLanguages());
+                $contentLang = $api['http.utils']->guessContentLang($request->query->get('locale'), $request->getLanguages());
 
-                $result = $api['dataaccess.mongoodm.utils']->get('\Blimp\Media\Documents\Media', $id, $contentLang, $_securityDomain, $user);
+                $result = $api['dataaccess.mongoodm.utils']->get($_resourceClass, $id, ['fields' => $field], $contentLang, $_securityDomain, $user, $_parentResourceClass, $_parentIdField, $parent_id);
 
-                $b = $result->getBucket();
-                if($b === $_bucket) {
-                    $file = $result->getFilePath();
+                if (!empty($result) && !empty($result[$field]) && ($index >= 0 || !empty($result[$field][$index]))) {
+                    $fileinfo = $index < 0 ? $result[$field] : $result[$field][$index];
 
-                    if(filter_var($file, FILTER_VALIDATE_URL)) {
-                        return new RedirectResponse($file);
-                    } else if(file_exists($api['media.physical.path'].'/'.$b.'/'.$file)) {
-                        return new BinaryFileResponse($result->getBasePath().'/'.$result->getBucket().'/'.$file);
-                    } else if(file_exists($api['media.physical.path'].'/'.$b.'/'.$file)) {
-                        return new BinaryFileResponse($api['media.physical.path'].'/'.$result->getBucket().'/'.$file);
+                    if (filter_var($fileinfo['file'], FILTER_VALIDATE_URL)) {
+                        return new RedirectResponse($fileinfo['file']);
+                    } else {
+                        try {
+                            $path = $fileinfo['bucket'].'/'.$fileinfo['file'];
+                            $type = $api['media.filesystems']['user']->getMimetype($path);
+
+                            if (strpos($type, 'image/') === 0) {
+                                // Setup Glide server
+                                $server = ServerFactory::create([
+                                    'source' => $api['media.filesystems']['user'],
+                                    'cache' => $api['media.filesystems']['thumb'],
+                                ]);
+
+                                $response = $server->getImageResponse($path, $request->query->all());
+                                return $response;
+                            } elseif ($api['media.filesystems']['user']->has($path)) {
+                                $cache_info = array(
+                                    'last_modified' => date_create()->setTimestamp($api['media.filesystems']['user']->getTimestamp($path)),
+                                    'max_age'       => 31536000,
+                                    'private'       => false,
+                                    'public'        => true,
+                                );
+                                
+                                $response = new Response();
+                                $response->setCache($cache_info);
+                                if($response->isNotModified($request)) {
+                                    return $response;
+                                }
+                                
+                                $response = new StreamedResponse();
+                                $response->setCache($cache_info);
+                                $response->setStatusCode(Response::HTTP_OK);
+                                $response->headers->set('Content-Type', $type);
+                                $response->headers->set('Content-Length', $api['media.filesystems']['user']->getSize($path));
+                                $response->setExpires(date_create()->modify('+1 years'));
+
+                                $content = $api['media.filesystems']['user']->readStream($path);
+
+                                $response->setCallback(function () use ($content) {
+                                  $out = fopen('php://output', 'wb');
+
+                                  stream_copy_to_stream($content, $out);
+
+                                  fclose($out);
+                                  fclose($content);
+                                });
+
+                                return $response;
+                            }
+                        } catch(\League\Flysystem\FileNotFoundException $e) {
+                            throw new BlimpHttpException(Response::HTTP_NOT_FOUND, 'Not found');
+                        }
                     }
                 }
 
-                throw new BlimpHttpException(Response::HTTP_NOT_FOUND, "Not found");
+                throw new BlimpHttpException(Response::HTTP_NOT_FOUND, 'Not found');
 
                 break;
 
             default:
-                throw new BlimpHttpException(Response::HTTP_METHOD_NOT_ALLOWED, "Method not allowed");
+                throw new BlimpHttpException(Response::HTTP_METHOD_NOT_ALLOWED, 'Method not allowed');
         }
     }
 }
